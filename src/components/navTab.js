90cee708baced9737ade955851766d87a39da87c
Hippolyte Bach <hbach@juniorisep.com>
2022-09-18 21:37:57 +0200

    refactor tab bar

---
+++ src/components/navTab.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+function NavTab({active, id, title, icon, to, handleToggle}) {
+    return (
+        <li className={`list ${active === id ? "active" : ""}`}>
+            <NavLink
+                className="a"
+                to={to}
+                onClick={() => {
+                    handleToggle(id);
+                }}
+            >
+                <span className="text">{title}</span>
+                <span className="icon">
+                <ion-icon name={icon}></ion-icon>
+              </span>
+                <div className={`indicator ${active === id? "active" : ""}`}></div>
+            </NavLink>
+        </li>
+    );
+}
+
+export default NavTab;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,105 +1,29 @@
 import React, { useState } from "react";
 import "../style/navbar.css";
-import { NavLink } from "react-router-dom";
-
+import NavTab from "./navTab";
 function Navbar() {
-  const [l1, setL1] = useState(true);
-  const [l2, setL2] = useState(false);
-  const [l3, setL3] = useState(false);
-  const [l4, setL4] = useState(false);
-  const [l5, setL5] = useState(false);
+  const [active, setActive] = useState("l1");
 
-  function handleToggle(l1, l2, l3, l4, l5) {
-    setL1(l1);
-    setL2(l2);
-    setL3(l3);
-    setL4(l4);
-    setL5(l5);
+
+  function handleToggle(state) {
+    setActive(state);
   }
 
   return (
     <div className="navigationContainer">
       <div className="navigation">
         <ul>
-          <li className={`list ${l1 ? "active" : ""}`}>
-            <NavLink
-              className="a"
-              to="/"
-              onClick={() => {
-                handleToggle(true, false, false, false, false);
-              }}
-            >
-              <span className="text">Accueil</span>
-              <span className="icon">
-                <ion-icon name="home-outline"></ion-icon>
-              </span>
-              <div className={`indicator ${l1 ? "active" : ""}`}></div>
-            </NavLink>
-          </li>
-          <li className={`list ${l2 ? "active" : ""}`}>
-            <NavLink
-              className="a"
-              to="/dashboard"
-              onClick={() => {
-                handleToggle(false, true, false, false, false);
-              }}
-            >
-              <span className="text">Dashboard</span>
-              <span className="icon">
-                <ion-icon name="apps-outline"></ion-icon>
-              </span>
-              <div className={`indicator ${l2 ? "active" : ""}`}></div>
-            </NavLink>
-          </li>
-          <li className={`list ${l3 ? "active" : ""}`}>
-            <NavLink
-              className="a"
-              to="/search"
-              onClick={() => {
-                handleToggle(false, false, true, false, false);
-              }}
-            >
-              <span className="text">Rechercher</span>
-              <span className="icon">
-                <ion-icon name="search-outline"></ion-icon>
-              </span>
-              <div className={`indicator ${l3 ? "active" : ""}`}></div>
-            </NavLink>
-          </li>
-          <li className={`list ${l4 ? "active" : ""}`}>
-            <NavLink
-              className="a"
-              to="/stats"
-              onClick={() => {
-                handleToggle(false, false, false, true, false);
-              }}
-            >
-              <span className="text">Statistiques</span>
-              <span className="icon">
-                <ion-icon name="stats-chart-outline"></ion-icon>
-              </span>
-              <div className={`indicator ${l4 ? "active" : ""}`}></div>
-            </NavLink>
-          </li>
-          <li className={`list ${l5 ? "active" : ""}`}>
-            <NavLink
-              className="a"
-              to="/login"
-              onClick={() => {
-                handleToggle(false, false, false, false, true);
-              }}
-            >
-              <span className="text">Login</span>
-              <span className="icon">
-                <ion-icon name="log-in-outline"></ion-icon>
-              </span>
-              <div className={`indicator ${l5 ? "active" : ""}`}></div>
-            </NavLink>
-          </li>
+          <NavTab handleToggle={handleToggle} active={active} id="l1" title="Accueil" icon="home-outline" to="/" />
+          <NavTab handleToggle={handleToggle} active={active} id="l2" title="Dashboard" icon="apps-outline" to="/dashboard" />
+          <NavTab handleToggle={handleToggle} active={active} id="l3" title="Rechercher" icon="search-outline" to="/search" />
+          <NavTab handleToggle={handleToggle} active={active} id="l4" title="Statistiques" icon="stats-chart-outline" to="/stats" />
+          <NavTab handleToggle={handleToggle} active={active} id="l5" title="Login" icon="log-in-outline" to="/login" />
+
         </ul>
       </div>
     </div>
   );
 }
+
 
 export default Navbar;
