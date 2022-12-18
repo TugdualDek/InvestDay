6adc28afcc0e038553c9d33928b23e4769f86ca9
Hippolyte Bach <hbach@juniorisep.com>
2022-12-18 15:09:58 +0100

    custom 404

---
+++ pages/404.js
@@ -0,0 +1,6 @@
+import { AppProps } from "next/app";
+import DashBoardLayout from "../components/layouts/DashBoard.layout";
+
+export default function Custom404() {
+  return <h1>404 - Page Not Found</h1>;
+}
