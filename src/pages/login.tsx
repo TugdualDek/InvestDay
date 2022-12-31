1f6780f6a36107046b1f3fa8ac8354f0362f111a
Tugdual de K <tuak61273@eleve.isep.fr>
2022-12-31 11:38:13 +0100

    new login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,8 +2,8 @@ import Head from "next/head";
 import { useState } from "react";
 import { useAuthentification } from "../context/AuthContext";
 import homeStyles from "../styles/Home.module.css";
-import loginStyles from "../styles/Login.module.css";
-export default function Login() {
+import loginStyles from "../styles/Loginv2.module.css";
+export default function Login_test() {
   const { login } = useAuthentification();
 
   const [email, setEmail] = useState("");
@@ -13,6 +13,14 @@ export default function Login() {
     console.log(email, password);
     login(email, password);
   };
+
+  const [toggleLogin, setToggle] = useState(false);
+
+  function toggleLoginState() {
+    setToggle((prevState) => !prevState);
+    console.log(toggleLogin);
+  }
+
   return (
     <>
       <Head>
@@ -22,28 +30,92 @@ export default function Login() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={homeStyles.pageContainer}>
-        <div className={loginStyles.loginContainer}>
-          <h1>Login</h1>
-          <div style={{ width: 400, justifySelf: "center" }}>
-            <form style={{ display: "flex", flexDirection: "column" }}>
-              <label htmlFor="email">Email</label>
-              <input
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                type="email"
-                name="email"
-                id="email"
-              />
-              <label htmlFor="password">Password</label>
-              <input
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                type="password"
-                name="password"
-                id="password"
-              />
-              <input type="submit" value="Submit" onClick={handleSubmit} />
-            </form>
+        <div className={loginStyles.container}>
+          <div
+            className={`${loginStyles.main} ${
+              toggleLogin ? loginStyles.active : ""
+            }`}
+          >
+            <div className={loginStyles.login}>
+              <form>
+                <label
+                  onClick={toggleLoginState}
+                  className={`${toggleLogin ? loginStyles.inactive : ""}`}
+                >
+                  Connexion
+                </label>
+                <input
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="email..."
+                />
+                <input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="mot de passe..."
+                />
+                <button
+                  type="submit"
+                  value="Submit"
+                  onClick={handleSubmit}
+                  className={loginStyles.button}
+                >
+                  Connexion
+                </button>
+              </form>
+            </div>
+            <div
+              className={`${loginStyles.signin} ${
+                toggleLogin ? loginStyles.active : ""
+              }`}
+            >
+              <form>
+                <label
+                  onClick={toggleLoginState}
+                  className={`${toggleLogin ? loginStyles.active : ""}`}
+                >
+                  Inscription
+                </label>
+                <input
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="text"
+                  name="nom"
+                  id="nom"
+                  placeholder="nom..."
+                />
+                <input
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="email..."
+                />
+                <input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="mot de passe..."
+                />
+                <button
+                  type="submit"
+                  value="Submit"
+                  onClick={handleSubmit}
+                  className={loginStyles.button}
+                >
+                  Inscription
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </main>
