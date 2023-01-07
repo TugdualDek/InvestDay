4a4d67ff252ccfd3ca2a56d31f753b8eb0170be8
Hippolyte Bach <hbach@juniorisep.com>
2023-01-07 17:14:31 +0100

    persistent login

--- src/components/NavBar.component.jsx
+++ src/components/NavBar.component.jsx
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import navBarStyles from "../styles/NavBar.module.css";
 import NavTab from "./NavTab.component";
 import logo from "src/public/assets/logo.webp";
-
+import { useAuthentification } from "../context/AuthContext";
 import Image from "next/image";
 function Navbar() {
+  const { logout } = useAuthentification();
   const [active, setActive] = useState("l1");
   function handleToggle(state) {
     setActive(state);
@@ -55,11 +56,11 @@ function Navbar() {
           to="/ranks"
         />
         <NavTab
-          handleToggle={handleToggle}
+          handleToggle={(id) => logout()}
           active={active}
           id="logout"
           title="Déconnexion"
-          to="/logout"
+          to="/login"
         />
       </ul>
       <div
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,6 +6,7 @@ const AuthContext = createContext({
   login: (fetch, email, password) => {},
   logout: () => {},
   user: {},
+  reLogin: () => false,
 });
 
 function AuthProvider({ children }) {
@@ -14,19 +15,43 @@ function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const router = useRouter();
 
+  useEffect(() => {
+    reLogin();
+  }, []);
+
+  async function reLogin() {
+    try {
+      const lastUser = JSON.parse(window.sessionStorage.getItem("lastUser"));
+      console.log("Test storage", lastUser);
+      if (lastUser.token) {
+        console.log("logging in with last user");
+        setUser(lastUser);
+        setIsAuthenticated(true);
+        router.push(router.asPath);
+        return true;
+      }
+    } catch (e) {
+      console.error(e);
+    }
+    return false;
+  }
   async function login(fetch, email, password) {
     let result = await fetch.post("/api/auth/login", { email, password });
     if (result?.email) {
       setIsAuthenticated(true);
       setUser(result);
+      console.log("logged in", result);
+      window.sessionStorage.setItem("lastUser", JSON.stringify(result));
       router.push("/");
     }
   }
 
   async function logout() {
     console.warn("logout");
-    window.sessionStorage.removeItem("refreshToken");
+    window.sessionStorage.removeItem("lastUser");
+
     setIsAuthenticated(false);
+    router.push("/login");
   }
 
   return (
@@ -36,6 +61,7 @@ function AuthProvider({ children }) {
         login,
         logout,
         user,
+        reLogin,
       }}
     >
       {children}
@@ -44,17 +70,17 @@ function AuthProvider({ children }) {
 }
 
 const ProtectRoute = ({ children }) => {
-  const { isAuthenticated } = useAuthentification();
+  const { isAuthenticated, reLogin } = useAuthentification();
   const router = useRouter();
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     console.log(isAuthenticated, router.asPath);
-    if (!isAuthenticated && router.asPath !== "/login") {
+    if (!isAuthenticated && router.asPath !== "/login" && !reLogin()) {
       router.push("/login");
     } else {
       setIsLoaded(true);
     }
-  });
+  }, [isAuthenticated, router.asPath]);
 
   return isLoaded ? children : "Loading...";
 };
