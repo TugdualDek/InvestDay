3c31fecc77b8780dad74deacd4fa6c71fa8939b6
Hippolyte Bach <79230352+HipppB@users.noreply.github.com>
2023-02-18 17:29:32 +0000

    desactivate transactions

--- src/pages/api/transactions/index.ts
+++ src/pages/api/transactions/index.ts
@@ -11,95 +11,88 @@ export default apiHandler(transactionByWallet);
 
 async function transactionByWallet(req: Request, res: NextApiResponse<any>) {
   //desactivate temporarly this endpoint
-  // return res.status(200).json({
-  //   message: "This endpoint is temporarly disabled",
-  // });
-  if (req.method !== "POST") {
-    throw `Method ${req.method} not allowed`;
-  }
-  // get wallet
-  const { amount, executed, adminPrice, walletId, symbol, selling } = req.body;
+  return res.status(200).json({
+    message: "This endpoint is temporarly disabled",
+  });
+  // if (req.method !== "POST") {
+  //   throw `Method ${req.method} not allowed`;
+  // }
+  // const { amount, executed, adminPrice, walletId, symbol, selling } = req.body;
 
-  // check if walletId is provided
-  if (!walletId) throw "Wallet id is required";
+  // if (!walletId) throw "Wallet id is required";
 
-  // on recupere le wallet avec les transactions a l'aide du service
+  // const wallet = await walletsService.find(walletId, true);
 
-  const wallet = await walletsService.find(walletId, true);
+  // if (!wallet) throw "Wallet not found";
+  // if (wallet?.userId !== req.auth.sub && !req.auth.isAdmin)
+  //   throw "You are not allowed to access this wallet";
+  // if (adminPrice && !req.auth.isAdmin)
+  //   throw "You are not allowed to set admin price";
+  // if (!amount || (!adminPrice && !symbol))
+  //   throw "Please provide amount and stockId or adminPrice";
+  // if (executed && !req.auth.isAdmin)
+  //   throw "You are not allowed to force execute transaction";
 
-  // Gestion des permissions et des exeptions
-  if (!wallet) throw "Wallet not found";
-  if (wallet?.userId !== req.auth.sub && !req.auth.isAdmin)
-    throw "You are not allowed to access this wallet";
-  if (adminPrice && !req.auth.isAdmin)
-    throw "You are not allowed to set admin price";
-  if (!amount || (!adminPrice && !symbol))
-    throw "Please provide amount and stockId or adminPrice";
-  if (executed && !req.auth.isAdmin)
-    throw "You are not allowed to force execute transaction";
+  // if (amount <= 0) throw "Amount must be greater than 0";
 
-  if (amount <= 0) throw "Amount must be greater than 0";
+  // if (adminPrice) {
+  //   return res
+  //     .status(200)
+  //     .json(
+  //       await walletsService.addMoney(
+  //         walletId,
+  //         parseFloat(adminPrice) * parseFloat(amount)
+  //       )
+  //     );
+  // }
 
-  if (adminPrice) {
-    return res
-      .status(200)
-      .json(
-        await walletsService.addMoney(
-          walletId,
-          parseFloat(adminPrice) * parseFloat(amount)
-        )
-      );
-  }
+  // // Get last stock price
+  // const clientIp = requestIp.getClientIp(req);
 
-  // Get last stock price
-  const clientIp = requestIp.getClientIp(req);
+  // if (!clientIp) throw new Error("No client IP found");
+  // const summary: any = await stockService.getLastPrice(
+  //   symbol,
+  //   req.auth.sub,
+  //   clientIp || ""
+  // );
+  // if (summary?.results[0]?.error == "NOT_FOUND") {
+  //   throw "Unknown symbol";
+  // }
+  // let stock = summary.results[0];
 
-  if (!clientIp) throw new Error("No client IP found");
-  const summary: any = await stockService.getLastPrice(
-    symbol,
-    req.auth.sub,
-    clientIp || ""
-  );
-  if (summary?.results[0]?.error == "NOT_FOUND") {
-    throw "Unknown symbol";
-  }
-  let stock = summary.results[0];
+  // if (!wallet.id) throw new Error("Wallet not found");
+  // const transaction = await transactionsService.create(
+  //   selling === "true",
+  //   symbol,
+  //   Number(parseFloat(amount).toFixed(1)),
+  //   wallet.id as number
+  // );
 
-  // Crete transaction PENDING
-  if (!wallet.id) throw new Error("Wallet not found");
-  const transaction = await transactionsService.create(
-    selling === "true",
-    symbol,
-    Number(parseFloat(amount).toFixed(1)),
-    wallet.id as number
-  );
+  // console.log("New transaction", transaction);
+  // if (stock.market_status !== "closed") {
+  //   if (selling === "true") {
+  //     let quantity = 0;
+  //     wallet.transactions.forEach((transaction: any) => {
+  //       if (transaction.symbol === symbol) {
+  //         quantity += (transaction.isSellOrder ? -1 : 1) * transaction.quantity;
+  //       }
+  //     });
+  //     if (quantity < parseFloat(amount)) {
+  //       await transactionsService.updateStatus(transaction.id, Status.FAILED);
+  //     } else {
+  //       await transactionsService.executeTransaction(transaction, stock.price);
+  //     }
+  //   } else {
+  //     if (wallet.cash < stock.price * parseFloat(amount)) {
+  //       await transactionsService.updateStatus(transaction.id, Status.FAILED);
+  //     } else {
+  //       await transactionsService.executeTransaction(transaction, stock.price);
+  //     }
+  //   }
+  // }
 
-  // Executed only if has money, market is open
-  console.log("New transaction", transaction);
-  if (stock.market_status !== "closed") {
-    if (selling === "true") {
-      let quantity = 0;
-      wallet.transactions.forEach((transaction: any) => {
-        if (transaction.symbol === symbol) {
-          quantity += (transaction.isSellOrder ? -1 : 1) * transaction.quantity;
-        }
-      });
-      if (quantity < parseFloat(amount)) {
-        await transactionsService.updateStatus(transaction.id, Status.FAILED);
-      } else {
-        await transactionsService.executeTransaction(transaction, stock.price);
-      }
-    } else {
-      if (wallet.cash < stock.price * parseFloat(amount)) {
-        await transactionsService.updateStatus(transaction.id, Status.FAILED);
-      } else {
-        await transactionsService.executeTransaction(transaction, stock.price);
-      }
-    }
-  }
+  // // get new wallet
+  // const newWallet = await walletsService.find(walletId);
 
-  // get new wallet
-  const newWallet = await walletsService.find(walletId);
-
-  return res.status(200).json(newWallet);
+  // return res.status(200).json(newWallet);
 }
