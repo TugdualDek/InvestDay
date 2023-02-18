0bed1c0917121fe4b8e64fa6c8565d82608f338f
Tugdual de K <tuak61273@eleve.isep.fr>
2023-02-18 12:06:23 +0100

    classement seulement des 10 premiers

--- src/components/TableRanks.component.jsx
+++ src/components/TableRanks.component.jsx
@@ -9,24 +9,24 @@ function TableRanks(props) {
       <div className={TableTransactionStyles.podium}>
         <div className={TableTransactionStyles.podiumItem}>
           <div className={TableTransactionStyles.podiumItemRank}>
-            1ere place :
-          </div>
-          <div className={TableTransactionStyles.podiumItemName}>
-            Wallet n°{data[0]?.id}
-          </div>
-          <div className={TableTransactionStyles.podiumItemScore}>
-            {data[0]?.publicWalletValue}$
-          </div>
-        </div>
-        <div className={TableTransactionStyles.podiumItem}>
-          <div className={TableTransactionStyles.podiumItemRank}>
             2eme place :
           </div>
           <div className={TableTransactionStyles.podiumItemName}>
             Wallet n°{data[1]?.id}
           </div>
           <div className={TableTransactionStyles.podiumItemScore}>
-            {data[1]?.publicWalletValue}$
+            {(data[1]?.publicWalletValue).toFixed(2)}$
+          </div>
+        </div>
+        <div className={TableTransactionStyles.podiumItem}>
+          <div className={TableTransactionStyles.podiumItemRank}>
+            1ere place :
+          </div>
+          <div className={TableTransactionStyles.podiumItemName}>
+            Wallet n°{data[0]?.id}
+          </div>
+          <div className={TableTransactionStyles.podiumItemScore}>
+            {(data[0]?.publicWalletValue).toFixed(2)}$
           </div>
         </div>
         <div className={TableTransactionStyles.podiumItem}>
@@ -37,7 +37,7 @@ function TableRanks(props) {
             Wallet n°{data[2]?.id}
           </div>
           <div className={TableTransactionStyles.podiumItemScore}>
-            {data[2]?.publicWalletValue}$
+            {(data[2]?.publicWalletValue).toFixed(2)}$
           </div>
         </div>
       </div>
@@ -55,8 +55,10 @@ function TableRanks(props) {
           {data.map(
             (item, index) =>
               // check if user is admin or not
-              // begin at 4 because the first 3 are in the podium
-              index > 2 && (
+              // begin at 4 because the first 3 are in the podium and end at 10
+              // because we only want to display the 10 first
+              index >= 3 &&
+              index < 10 && (
                 // if not admin, display the user
                 <tr
                   key={index}
