ab13d58d3136fa0829e4b3939050277575e1a4a6
Hippolyte Bach <79230352+HipppB@users.noreply.github.com>
2023-02-21 18:22:06 +0100

    Update forceTransactions.ts

--- src/pages/api/validation/forceTransactions.ts
+++ src/pages/api/validation/forceTransactions.ts
@@ -1,8 +1,8 @@
 import { apiHandler } from "../../../helpers/api/api-handler";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Request } from "../../../types/request.type";
-import { PrismaClient } from "@prisma/client";
-// import { prisma } from "../../../lib/prisma";
+import { PrismaClient, Status } from "@prisma/client";
+import { prisma } from "../../../lib/prisma";
 
 import requestIp from "request-ip";
 import stocksService from "../../../services/stocks/stocks.service";
@@ -18,7 +18,7 @@ async function validateTransactions(req: Request, res: NextApiResponse<any>) {
   }
   if (!req.auth.isAdmin) throw "You are not allowed to force transactions";
 
-  let prisma = new PrismaClient();
+  //   let prisma = new PrismaClient();
 
   const transactions = await prisma.transaction.findMany({
     where: {
@@ -46,38 +46,52 @@ async function validateTransactions(req: Request, res: NextApiResponse<any>) {
   transactions.forEach(async (transaction) => {
     //check if lastStock.symbol is not undefnied and if it is equal to transaction.symbol
     //if it is equal to transaction.symbol then return
-    const price: any = await stocksService.getLastPrice(
-      transaction.symbol,
-      req.auth.sub,
-      clientIp as string
-    );
+
     if (!transaction.isSellOrder) {
       //wallet :
-
+      console.log("transaction", transaction);
       const wallet = await prisma.wallet.findUnique({
         where: {
           id: transaction.wallet.id,
         },
       });
+      console.log("wallet", wallet);
       if (!wallet) throw new Error("Wallet not found");
-      prisma.wallet.update({
+
+      const price: any = await stocksService.getLastPrice(
+        transaction.symbol,
+        req.auth.sub,
+        clientIp as string
+      );
+      console.log("price", price);
+      console.log(
+        "newcash",
+        wallet.cash + price.results[0].price * transaction.quantity
+      );
+      const newWallet = await prisma.wallet.update({
         where: {
-          id: transaction.wallet.id,
+          id:
+            typeof transaction.wallet.id == "string"
+              ? parseInt(transaction.wallet.id)
+              : transaction.wallet.id,
         },
         data: {
-          cash: wallet.cash + price * transaction.quantity,
+          cash: wallet.cash + price.results[0].price * transaction.quantity,
         },
       });
-      prisma.transaction.update({
+
+      console.log("newWallet", newWallet);
+      const newtransactio = await prisma.transaction.update({
         where: {
           id: transaction.id,
         },
         data: {
           status: "EXECUTED",
-          valueAtExecution: price,
+          valueAtExecution: price.results[0].price,
           executedAt: new Date(),
         },
       });
+      console.log("newtransactio", newtransactio);
     }
   });
 
