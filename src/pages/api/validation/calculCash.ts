939203dff35dafb476c1d0a32d5c605125e703f4
Hippolyte Bach <79230352+HipppB@users.noreply.github.com>
2023-02-22 14:16:16 +0000

    admin only

--- src/pages/api/validation/calculCash.ts
+++ src/pages/api/validation/calculCash.ts
@@ -10,36 +10,35 @@ import stocksService from "../../../services/stocks/stocks.service";
 export default apiHandler(updatePublicValue);
 
 async function updatePublicValue(req: Request, res: NextApiResponse<any>) {
-    if (req.method !== "GET") {
-        throw `Method ${req.method} not allowed`;
-    }
-    //if (!req.auth.isAdmin) throw "You are not allowed to log values of wallets";
+  if (req.method !== "GET") {
+    throw `Method ${req.method} not allowed`;
+  }
+  if (!req.auth.isAdmin) throw "You are not allowed to log values of wallets";
 
-    const clientIp = requestIp.getClientIp(req);
-    if (!clientIp) throw new Error("No client IP found");
+  const clientIp = requestIp.getClientIp(req);
+  if (!clientIp) throw new Error("No client IP found");
 
-    // get all the wallets and iterate through their transactions
-    // remove cash if transaction is a buy, add cash if transaction is a sell
-    // then update the wallet with the new cash value
+  // get all the wallets and iterate through their transactions
+  // remove cash if transaction is a buy, add cash if transaction is a sell
+  // then update the wallet with the new cash value
 
-    const wallets = await walletsService.getAllWallets();
-    for (const wallet of wallets) {
-        let cash = 10000;
-        for (const transaction of wallet.transactions) {
-            if(transaction.status === "EXECUTED"){
-                //check if valueAtExecution is null
-                if(transaction.valueAtExecution === null) continue;
-                if(transaction.isSellOrder === false){
-                    cash -= transaction.valueAtExecution * transaction.quantity;
-                } else if(transaction.isSellOrder === true){
-                    cash += transaction.valueAtExecution * transaction.quantity;
-                }
-            }
+  const wallets = await walletsService.getAllWallets();
+  for (const wallet of wallets) {
+    let cash = 10000;
+    for (const transaction of wallet.transactions) {
+      if (transaction.status === "EXECUTED") {
+        //check if valueAtExecution is null
+        if (transaction.valueAtExecution === null) continue;
+        if (transaction.isSellOrder === false) {
+          cash -= transaction.valueAtExecution * transaction.quantity;
+        } else if (transaction.isSellOrder === true) {
+          cash += transaction.valueAtExecution * transaction.quantity;
         }
-        //update wallet with new cash value
-        await walletsService.updateCash(wallet.id, cash);
+      }
     }
-    
+    //update wallet with new cash value
+    await walletsService.updateCash(wallet.id, cash);
+  }
 
-    return res.status(200).json({ message: "Cash calculated" });
+  return res.status(200).json({ message: "Cash calculated" });
 }
