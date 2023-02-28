c1077d4cbf4670b4b26b5088c832ef814cc656b2
Hippolyte Bach <79230352+HipppB@users.noreply.github.com>
2023-02-28 18:57:41 +0000

    order by asc

--- src/services/transactions/transactions.service.ts
+++ src/services/transactions/transactions.service.ts
@@ -19,7 +19,7 @@ async function findAll(
   } else {
     return await prisma.transaction.findMany({
       where: { walletId: parseInt(walletId) },
-      orderBy: { executedAt: "desc" },
+      orderBy: { executedAt: "asc" },
     });
   }
 }
